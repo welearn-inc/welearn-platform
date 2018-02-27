6e2e9a6ec3e74858a5ad1c67d0cfd73dd0b1298c
Nikita <hello@nikitadanilov.com>
2018-02-27 12:27:08 -0800

    Migrations added

--- WeLearn/settings/base.py
+++ WeLearn/settings/base.py
@@ -3,19 +3,11 @@ import datetime
 
 from WeLearn.restconf.main import *
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'AAs65snz9+3vnhjmr3hijb0u@&w68t#5_e8s9-lbfhv-'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Application definition
 
--- WeLearn/settings/local.py
+++ WeLearn/settings/local.py
@@ -1,3 +1,11 @@
 from WeLearn.settings.base import *
 
 # Override base.py settings here
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'AAs65snz9+3vnhjmr3hijb0u@&w68t#5_e8s9-lbfhv-'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
